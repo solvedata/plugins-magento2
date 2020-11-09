3b399b3a1e8872635ac02e0bc017f95121628513
Liam O'Neill <liam@solvedata.io>
2020-11-10 09:56:45 +1300

    Support MAGENTO_PATH & MAGENTO_VERSION environment variables in install.sh script

--- install.sh
+++ install.sh
@@ -2,40 +2,35 @@
 
 TMP_DIR=$(mktemp -d)
 
-# Start an SSH agent session and add all keys to into it
-eval "$(ssh-agent -s)"
-find ~/.ssh -maxdepth 1 -name 'id_*' -not -name 'id_*.pub' -exec ssh-add {} \;
-
 run_composer() {
-  # Add github's host key to composer's known hosts file if it doesn't exist
-  test -f "${TMP_DIR}/ssh_known_hosts" \
-    || echo 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' > "${TMP_DIR}/ssh_known_hosts"
-  
-  # See https://hub.docker.com/_/composer for details around
-  #   using a non-root user and mounting the SSH agent socket.
+  # See https://hub.docker.com/_/composer for details around using a non-root user.
   docker run --rm --interactive --tty \
-    --env SSH_AUTH_SOCK=/ssh-auth.sock \
     --volume "${COMPOSER_HOME:-$HOME/.composer}:/tmp" \
     --volume "${PWD}:/app" \
-    --volume "${SSH_AUTH_SOCK}:/ssh-auth.sock" \
-    --volume "${TMP_DIR}/ssh_known_hosts:/etc/ssh/ssh_known_hosts" \
     --volume /etc/passwd:/etc/passwd:ro \
     --volume /etc/group:/etc/group:ro \
     --user "$(id -u):$(id -g)" \
     composer:1.8.0 $1
 }
 
-echo "Enter folder name [magento]:"
-read MAGENTO_PATH
-MAGENTO_PATH="${MAGENTO_PATH:-magento}"
-mkdir $MAGENTO_PATH
-cd $MAGENTO_PATH
+if [[ -z "${MAGENTO_PATH:-}" ]]; then
+  echo "Enter folder name [magento]:"
+  read -r MAGENTO_PATH
+fi
 
-if [ ! -f "composer.json" ]; then
+MAGENTO_PATH="${MAGENTO_PATH:-magento}"
+mkdir -p -- "${MAGENTO_PATH}"
+cd -- "${MAGENTO_PATH}" || exit 1
+
+if [[ ! -f "composer.json" ]]; then
   echo "Start of installing magento..."
-  echo "Enter magento version: "
-  read MAGENTO_VERSION
-  if [ -z "$MAGENTO_VERSION" ]; then
+
+  if [[ -z "${MAGENTO_VERSION:-}" ]]; then
+    echo "Enter magento version: "
+    read -r MAGENTO_VERSION
+  fi
+
+  if [[ -z "$MAGENTO_VERSION" ]]; then
     MAGENTO_COMPOSER="magento/project-community-edition"
   else
     MAGENTO_COMPOSER="magento/project-community-edition=$MAGENTO_VERSION"
@@ -53,11 +48,7 @@ if [ ! -f "composer.json" ]; then
 
   echo "Downloading solvedata package..."
 
-  # Require solvedata/plugins-magento2 package with the --no-interaction flag so it will use SSH to clone the repo
-  #   rather than prompting for a Github API token.
-  # This requires setting up a readonly deploy SSH key in the solvedata/plugins-magento2 repo.
-  run_composer "config repositories.repo-name vcs git@github.com:solvedata/plugins-magento2.git"
-  run_composer "require --no-interaction --ignore-platform-reqs solvedata/plugins-magento2"
+  run_composer "require --ignore-platform-reqs solvedata/plugins-magento2"
 
   echo "Remove installer temp files"
   rm -R "${TMP_DIR}"
