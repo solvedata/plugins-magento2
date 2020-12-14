1c9997a278bd28c2f8017e43147bb8ecc12b52dc
Liam O'Neill <liam@solvedata.io>
2020-12-15 11:55:52 +1300

    Convert guest customer's carts when the anonymous carts feature is enabled

--- Model/Event/Transport/Adapter/GraphQL/Mutation/SalesOrderSaveAfter/ConvertCart.php
+++ Model/Event/Transport/Adapter/GraphQL/Mutation/SalesOrderSaveAfter/ConvertCart.php
@@ -18,6 +18,25 @@ mutation convertCart($id: String!, $orderId: String, $provider: String!) {
 GRAPHQL;
 
     /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * @param Config $config
+     * @param PayloadConverter $payloadConverter
+     * @param array $data
+     */
+    public function __construct(
+        Config $config,
+        PayloadConverter $payloadConverter,
+        array $data = []
+    ) {
+        $this->config = $config;
+        parent::__construct($payloadConverter, $data);
+    }
+
+    /**
      * Mutation is allowed
      *
      * @return bool
@@ -25,10 +44,27 @@ GRAPHQL;
     public function isAllowed(): bool
     {
         $payload = $this->getEvent()['payload'];
-        if (empty($payload['order'][OrderInterface::EXTENSION_ATTRIBUTES_KEY]['is_object_new'])
-            || !empty($payload['order'][OrderInterface::CUSTOMER_IS_GUEST])
-            || empty($payload['order'][OrderInterface::REMOTE_IP])
-        ) {
+        $order = $payload['order'];
+
+        // The `is_object_new` extension attribute is set by the plugin on all new orders.
+        // If the attribute is missing this indicates that the order pre-dates the plugin
+        //      and therefore no cart will exist in Solve.
+        $historicOrder = empty($order[OrderInterface::EXTENSION_ATTRIBUTES_KEY]['is_object_new']);
+        if ($historicOrder) {
+            return false;
+        }
+
+        // The absence of an order's `remote_ip` field is inferred to mean that the order wasn't created by
+        //      a human and therefore there no associated cart would have been created.
+        $automatedOrder = empty($order[OrderInterface::REMOTE_IP]);
+        if ($automatedOrder) {
+            return false;
+        }
+
+        // Only attempt to convert carts for guest users if the plugin has been configured to create
+        //      carts for anonymous customers.
+        $guestOrder = !empty($order[OrderInterface::CUSTOMER_IS_GUEST]);
+        if ($guestOrder && $this->config->isAnonymousCartsEnabled() === false) {
             return false;
         }
 
@@ -48,7 +84,7 @@ GRAPHQL;
 
         return [
             'id'       => $payload['order'][OrderInterface::QUOTE_ID],
-            'orderId' => $payload['order'][OrderInterface::INCREMENT_ID],
+            'orderId'  => $payload['order'][OrderInterface::INCREMENT_ID],
             'provider' => $this->payloadConverter->getOrderProvider($payload['area']),
         ];
     }
