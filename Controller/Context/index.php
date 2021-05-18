db6ee0e2f84b0c3327766d5026c9a0face5a5f98
Liam O'Neill <liam@solvedata.io>
2021-05-18 19:42:49 +1200

    Add a frontend endpoint to retrieve the current sessions cart id

---
+++ Controller/Context/index.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SolveData\Events\Controller\Context;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $checkoutSession;
+    protected $jsonHelper;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Framework\Json\Helper\Data $jsonHelper
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Json\Helper\Data $jsonHelper,
+        \Magento\Checkout\Model\Session $checkoutSession
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->jsonHelper = $jsonHelper;
+
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $quote = $this->checkoutSession->getQuote();
+        $context = [
+            "qouteId" => $quote->getId()
+        ];
+
+        $this->getResponse()->representJson(
+            $this->jsonHelper->jsonEncode($context)
+        );
+    }
+}
