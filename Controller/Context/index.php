6b7af97b8c41e5a0e93f7692f248d6166b9a21e7
Dylan Chong <dylan.chong@icloud.com>
2021-06-04 14:42:51 +1200

    Add store and masked quote id

--- Controller/Context/index.php
+++ Controller/Context/index.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace SolveData\Events\Controller\Context;
 
+use SolveData\Events\Model\Logger;
+
 class Index extends \Magento\Framework\App\Action\Action
 {
     protected $checkoutSession;
     protected $jsonHelper;
+    protected $storeManager;
+    protected $logger;
+    protected $websiteRepository;
+    protected $quoteIdToMaskedQuoteId;
 
     /**
      * @param \Magento\Framework\App\Action\Context $context
@@ -17,23 +23,47 @@ class Index extends \Magento\Framework\App\Action\Action
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\Json\Helper\Data $jsonHelper,
-        \Magento\Checkout\Model\Session $checkoutSession
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Store\Model\WebsiteRepository $websiteRepository,
+        \Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface $quoteIdToMaskedQuoteId,
+        Logger $logger
     ) {
         $this->checkoutSession = $checkoutSession;
         $this->jsonHelper = $jsonHelper;
+        $this->storeManager = $storeManager;
+        $this->logger = $logger;
+        $this->websiteRepository = $websiteRepository;
+        $this->quoteIdToMaskedQuoteId = $quoteIdToMaskedQuoteId;
 
         parent::__construct($context);
     }
 
     public function execute()
     {
-        $quote = $this->checkoutSession->getQuote();
+        $quoteId = $this->checkoutSession->getQuote()->getId();
+        $quoteIdMasked = empty($quoteId) ? null : $this->getQuoteMaskId((int) $quoteId);
+
         $context = [
-            "quoteId" => $quote->getId()
+            "quoteId" => $quoteId,
+            "quoteIdMasked" => $quoteIdMasked,
+            "store" => $this->getStore()
         ];
 
         $this->getResponse()->representJson(
             $this->jsonHelper->jsonEncode($context)
         );
+    }
+
+    private function getStore()
+    {
+        $websiteId = $this->storeManager->getStore()->getWebsiteId();
+        $website = $this->websiteRepository->getById($websiteId);
+        return $website->getCode();
+    }
+
+    private function getQuoteMaskId($quoteId)
+    {
+        return $this->quoteIdToMaskedQuoteId->execute($quoteId);
     }
 }
