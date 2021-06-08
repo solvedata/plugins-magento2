3fb4c9a833c26283ec0e4f0ebbfb2098574ce595
Liam O'Neill <liam@solvedata.io>
2021-06-08 17:34:19 +1200

    Add a frontend endpoint to retrieve the current sessions cart id (#47)
    Co-authored-by: Dylan Chong <dylan.chong@icloud.com>

---
+++ Controller/Context/index.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SolveData\Events\Controller\Context;
+
+use SolveData\Events\Model\Logger;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    private $checkoutSession;
+    private $jsonHelper;
+    private $storeManager;
+    private $quoteIdToMaskedQuoteId;
+    private $logger;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Framework\Json\Helper\Data $jsonHelper
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface $quoteIdToMaskedQuoteId
+     * @param Logger
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Json\Helper\Data $jsonHelper,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface $quoteIdToMaskedQuoteId,
+        Logger $logger
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->jsonHelper = $jsonHelper;
+        $this->storeManager = $storeManager;
+        $this->quoteIdToMaskedQuoteId = $quoteIdToMaskedQuoteId;
+        $this->logger = $logger;
+
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $quoteId = $this->checkoutSession->getQuote()->getId();
+        $quoteIdMasked = $this->getQuoteMaskId($quoteId);
+
+        $context = [
+            "quoteId" => $quoteId,
+            "quoteIdMasked" => $quoteIdMasked,
+            "store" => $this->getStore()
+        ];
+
+        $this->getResponse()->representJson(
+            $this->jsonHelper->jsonEncode($context)
+        );
+    }
+
+    private function getStore()
+    {
+        $website = $this->storeManager->getWebsite();
+        return $website->getCode();
+    }
+
+    private function getQuoteMaskId($quoteIdString)
+    {
+        try {
+            $quoteId = (int) $quoteIdString;
+            if (empty($quoteId)) return null;
+
+            // Note this returns an empty string when a mask id does not exist (e.g. when the user is signed in).
+            return $this->quoteIdToMaskedQuoteId->execute($quoteId);
+        } catch (\Throwable $t) {
+            $this->logger->error($t);
+            return null;
+        }
+    }
+}
