26d91806afd38a56d6e5519af27dcf9175ed8bd2
Liam O'Neill <liam@solvedata.io>
2021-06-09 19:07:58 +1200

    Set occurred_at for order & create mutations

--- Model/Event/Transport/Adapter/GraphQL/Mutation/CheckoutCartSaveAfter.php
+++ Model/Event/Transport/Adapter/GraphQL/Mutation/CheckoutCartSaveAfter.php
@@ -7,8 +7,8 @@ namespace SolveData\Events\Model\Event\Transport\Adapter\GraphQL\Mutation;
 class CheckoutCartSaveAfter extends MutationAbstract
 {
     const QUERY = <<<'GRAPHQL'
-mutation createOrUpdateCart($input: CartInput!) {
-    createOrUpdateCart(input: $input) {
+mutation createOrUpdateCart($input: CartInput!, $options: CreateOrUpdateCartOptions!) {
+    createOrUpdateCart(input: $input, options: $options) {
         profile_id
     }
 }
@@ -31,8 +31,19 @@ GRAPHQL;
             $payload['quoteAllVisibleItems'],
             $payload['area']
         );
-        $input['lastInteractionAt'] = $this->payloadConverter->getFormattedDatetime($event['created_at']);
 
-        return ['input' => $input];
+        // Use the timestamp of when the event was enqueued as the event's "occurred at" time.
+        // Note that this the time when the event was created not when the cart was created.
+        $occurredAt = $this->payloadConverter->getFormattedDatetime($event['created_at']);
+
+        $input['lastInteractionAt'] = $occurredAt;
+        $options = [
+            'occurred_at' => $occurredAt
+        ];
+
+        return [
+            'input' => $input,
+            'options' => $options
+        ];
     }
 }
