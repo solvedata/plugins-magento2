8e350525cc76505f8c781874e329c339eb7c12be
Liam O'Neill <liam@solvedata.io>
2021-08-19 17:49:31 +1200

    Get rid of the unnecessary multiRequest function

--- Model/Event/Transport/Adapter/CurlAbstract.php
+++ Model/Event/Transport/Adapter/CurlAbstract.php
@@ -10,23 +10,6 @@ use SolveData\Events\Api\Event\Transport\AdapterInterface;
 abstract class CurlAbstract extends CurlAdapter implements AdapterInterface
 {
     /**
-     * Get default options
-     *
-     * @return array
-     */
-    private function getDefaultConfig(): array
-    {
-        $config = [];
-        foreach (array_keys($this->_config) as $param) {
-            if (array_key_exists($param, $this->_allowedParams)) {
-                $config[$this->_allowedParams[$param]] = $this->_config[$param];
-            }
-        }
-
-        return $config;
-    }
-
-    /**
      * Send events by bulk
      *
      * @param array $events
@@ -54,64 +37,4 @@ abstract class CurlAbstract extends CurlAdapter implements AdapterInterface
      * @return int|null
      */
     abstract protected function request(array $event);
-
-    /**
-     * Curl_multi_* requests support
-     *
-     * @param array $urls
-     * @param array $options
-     *
-     * @return array
-     */
-    public function multiRequest($urls, $options = []): array
-    {
-        $handles = [];
-        $result = [];
-
-        $multihandle = curl_multi_init();
-
-        // add default parameters
-        foreach ($this->getDefaultConfig() as $defaultOption => $defaultValue) {
-            if (!isset($options[$defaultOption])) {
-                $options[$defaultOption] = $defaultValue;
-            }
-        }
-
-        foreach ($urls as $key => $url) {
-            $handles[$key] = curl_init();
-            curl_setopt($handles[$key], CURLOPT_URL, $url);
-            curl_setopt($handles[$key], CURLOPT_HEADER, 0);
-            curl_setopt($handles[$key], CURLOPT_RETURNTRANSFER, 1);
-            /** @customization START */
-            if (!empty($options[$key])) {
-                curl_setopt_array($handles[$key], $options[$key]);
-            }
-            /** @customization END */
-            curl_multi_add_handle($multihandle, $handles[$key]);
-        }
-        $process = null;
-        do {
-            curl_multi_exec($multihandle, $process);
-            usleep(100);
-        } while ($process > 0);
-
-        foreach ($handles as $key => $handle) {
-            /** @customization START */
-            $result[$key] = [
-                'request' => [
-                    'url'        => $urls[$key],
-                    'parameters' => urldecode($options[$key][CURLOPT_POSTFIELDS])
-                ],
-                'response' => [
-                    'body' => curl_multi_getcontent($handle),
-                    'code' => curl_getinfo($handle, CURLINFO_HTTP_CODE),
-                ],
-            ];
-            /** @customization END */
-            curl_multi_remove_handle($multihandle, $handle);
-        }
-        curl_multi_close($multihandle);
-
-        return $result;
-    }
 }
